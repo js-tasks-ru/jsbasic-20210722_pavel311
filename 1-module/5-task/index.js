b2ca66e970f421b562f66f5cd4d27d24dcd6226a
Paul Turta <das1254336@gmail.com>
2021-08-05 17:06:49 +0300

    решения задач2

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,8 +1,7 @@
 function truncate(str, maxlength) {
     if (str.length > maxlength){
-    let shortedStr = str.slice(0, maxlength);
-    return shortedStr + '‘&hellip;'
-    ;
+    let shortedStr = str.slice(0, maxlength - 1);
+    return shortedStr + `…`;
     }
     else{
         return str;
