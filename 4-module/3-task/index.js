837caecacfcb2495fee03a071c1a0fb6f4d170d6
Paul Turta <das1254336@gmail.com>
2021-09-01 16:33:04 +0300

    5й модуль

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,21 +1,23 @@
 function highlight(table) {
-for (let tr of table.rows){
-  if (tr.cells[3].outerText != "Status"){
-  if (!tr.cells[3].dataset.available){
-    tr.hidden = 'true';
+  for (let tr of table.rows){
+    if (tr.cells[3].outerText != "Status"){
+    if (!tr.cells[3].dataset.available){
+      tr.hidden = 'true';
+    }
+      else {
+        let className = (tr.cells[3].dataset.available === 'true') ? ' available' : ' unavailable';
+        tr.className = tr.className + className;
+      }
+    }
+    if(tr.cells[2].outerText == 'm'){
+      tr.className = tr.className + ' male';
+    }
+    else if(tr.cells[2].outerText == 'f'){
+      tr.className = tr.className + ' female';
+    }
+    if(tr.cells[1].outerText < 18){
+      tr.style.textDecoration = "line-through";
+    }
+  
   }
-  else
-  tr.className = (tr.cells[3].dataset.available === 'true') ? 'available' : 'unavailable';
-}
-  if(tr.cells[2].outerText == 'm'){
-    tr.className = 'male';
   }
-  else if(tr.cells[2].outerText == 'f'){
-    tr.className = 'female';
-  }
-  if(tr.cells[1].outerText < 18){
-    tr.style="text-decoration: line-through";
-  }
-
-}
-}
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let toggleButton = document.querySelector(`.toggle-text-button`);
+  let text = document.querySelector('#text');
+
+  toggleButton.addEventListener('click', () => {
+    if (text.hidden == true){
+      text.hidden = false;
+    }
+    else {
+      text.hidden = true;
+    }
+  })
 }
