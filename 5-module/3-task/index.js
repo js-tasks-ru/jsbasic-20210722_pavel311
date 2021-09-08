fe08b1e9355b784a4076969ec7da5e018da4b017
Paul Turta <das1254336@gmail.com>
2021-09-08 14:43:06 +0300

    Проект (пролог)

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,47 @@
 function initCarousel() {
-  // ваш код...
+const inner = document.querySelector('.carousel__inner');
+const slide = document.querySelector('.carousel__slide');
+const slideWidth = slide.offsetWidth;
+const nextButton = document.querySelector('.carousel__arrow_right');
+const prevButton = document.querySelector('.carousel__arrow_left');
+let transition = 0;
+let currentSlide = 0;
+
+
+
+
+nextButton.addEventListener('click', () => {
+  transition -= slideWidth; 
+  setTransition();
+  if (currentSlide < 3) { 
+    currentSlide++;
 }
+//console.log(currentSlide); логирование  
+
+});
+
+prevButton.addEventListener('click', () => {
+  transition += slideWidth; 
+  setTransition();
+  if (currentSlide > 0) { 
+    currentSlide--; 
+  //console.log(currentSlide); логирование  
+}
+});
+
+ setTransition = () => {
+  inner.style.transform = `translateX(${transition}px)`;
+};
+
+сheckButtons = () => {
+  if(currentSlide === 0){
+    prevButton.style.display = 'none';
+  }
+  else if(currentSlide === 3){
+    nextButton.style.display = 'none';
+  }
+};
+сheckButtons();
+}
+
+
