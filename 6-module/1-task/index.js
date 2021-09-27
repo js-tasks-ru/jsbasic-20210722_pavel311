4f8d7fca18fea106c15f7bacb48c5195926c45ca
Paul Turta <das1254336@gmail.com>
2021-09-27 14:16:17 +0300

    .

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,22 +1,43 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
+
 export default class UserTable {
   constructor(rows) {
     this.rows = rows;
     this.elem = document.querySelector('table');
     this.removeLine();
+    this.elem = document.createElement('table');
+
+    this.elem.innerHTML = `
+      <thead>
+          <tr>
+            <td>Имя</td>
+            <td>Возраст</td>
+            <td>Зарплата</td>
+            <td>Город</td>
+            <td></td>
+          </tr>
+      </thead>
+    `;
+
+    let tableInner = rows.map(row => {
+      let cellsWithData = Object.values(row) // для каждого значения из объекта row
+        .map(value => `<td>${value}</td>`) // обернуть его в <td>
+        .join(''); // полученный массив <td>...</td> объединить в одну строку
+
+      return `
+          <tr>
+            ${cellsWithData}
+            <td><button>X</button></td>
+          </tr>
+        `; // возвращаем верстку одной строки
+    }).join('');
+
+    this.elem.innerHTML += `
+      <tbody>
+        ${tableInner}
+      <tbody>
+    `; // оборачиваем полученные строчки в tbody
+
+    this.elem.addEventListener('click', (event) => this.onClick(event));
   }
      removeLine(){
       const buttons = this.elem.querySelectorAll('button');
@@ -25,8 +46,19 @@ export default class UserTable {
           event.target.closest('tr').remove();
         });
       }
+
+  onClick(event) {
+    if (event.target.tagName != 'BUTTON') {
+      return;
+    }
+
+    let tr = event.target.closest('tr');
+
+    tr.remove();
   }
       createRows(){
-        
+
       }
+} 
+
 }
