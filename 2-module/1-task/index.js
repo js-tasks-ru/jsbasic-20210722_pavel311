edf4c021e80ff40229ce94561ef2e7c0a0422821
Paul Turta <das1254336@gmail.com>
2021-10-25 23:19:12 +0300

    8 module 1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,9 +1,9 @@
 function sumSalary(salaries) {
- let sum = 0;
- for (let key of Object.values(salaries)){
-   if (Number.isInteger(key)){
-     sum += key;
-     }
-   }
- return sum;
-}
+  let sum = 0;
+  for (let key of Object.values(salaries)){
+    if (Number.isInteger(key)){
+      sum += key;
+      }
+    }
+  return sum;
+ }
